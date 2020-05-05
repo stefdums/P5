f7d1b5baa06a8333c7410cf1bb9ce82c92984a4c
stefdums <stephane-dumont@hotmail.fr>
2020-05-05 14:42:46 +0200

    v 0.2

--- meublesEnChene.js
+++ meublesEnChene.js
@@ -1,40 +1,43 @@
-   
-let xhr = new XMLHttpRequest();
+
 let listeMeubles = document.getElementById("liste-meubles")
 let idMeuble
 
-xhr.onreadystatechange = function(){
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200){
-        let results = JSON.parse(this.responseText);
-        console.log(results)
- 
-        
-//creation de la boucle, 
-              
-        for (let i = 0; i < results.length; i++){
+const getFurnitures = async function(){
+    try{
+        let response = await fetch("http://localhost:3000/api/furniture")//renvoi une promesse
+        if(response.ok){//status = 200->299
+            let results = await response.json()//renvoi une promesse (converti en json)
+            //console.log(results)
+
+            for (let i = 0; i < results.length; i++){
          
-            idMeuble = results[i]._id
-            let nomDuMeuble = results[i].name
+                idMeuble = results[i]._id
+                let nomDuMeuble = results[i].name
+                
+            //création d'un a avec href="meuble.html" pour chaque article,            
+                let lien = document.createElement("a");
+                lien.id = results[i].name;
+                let adressLien = "meuble.html";
+                lien.setAttribute("href", "meuble.html?id=" +idMeuble)
+                //lien.setAttribute("target", "_blank") 
+                lien.textContent = nomDuMeuble
+            //création d'un li pour chaque article
+                let li = document.createElement("li");
+                li.classList.add("liste-meuble")
+            //ajout du a dans le li
+                li.appendChild(lien)
+                listeMeubles.appendChild(li)
             
-//création d'un a avec href="meuble.html" pour chaque article,            
-            let lien = document.createElement("a");
-            lien.id = results[i].name;
-            let adressLien = "meuble.html";
-            lien.setAttribute("href", "meuble.html?id=" +idMeuble)
-            //lien.setAttribute("target", "_blank") 
-            lien.textContent = nomDuMeuble
-//création d'un li pour chaque article
-            let li = document.createElement("li");
-            li.classList.add("liste-meuble")
-//ajout du a dans le li
-            li.appendChild(lien)
-            listeMeubles.appendChild(li)
-
-        }          
-    }       
-};
+            }
+        }else{
+            console.error("Retour du serveur: ", response.status)
+        } 
+    }catch(e){
+        console.error(e + " probleme avec le fetch, promesse rejetée") // si resolution de la promesse est une erreur
+    }
+       
+}
+getFurnitures()
 
 
-xhr.open("GET", "http://localhost:3000/api/furniture");
-xhr.send();
-
+    
--- page-meuble.js
+++ page-meuble.js
@@ -4,129 +4,118 @@ let url = window.location.search;
 let searchUrl = new URLSearchParams(url);
 // recuperation de la valeur associée à id
 let id = searchUrl.get("id");
-
-let request = new XMLHttpRequest()
-
-// recuperation de la page meuble.html 
-let pageMeuble = document.querySelector("#page-meuble")
+ 
 let arrayMeuble = [];
 let arrayListe = [];
 
-request.onreadystatechange = function(){
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200){
-        let meuble = JSON.parse(this.responseText);
-        console.log(meuble)
-//selection des elements        
-        let h1Meuble = document.querySelector("#id-titre");
-        let imageMeuble = document.querySelector("#img")
-        let description = document.querySelector("#description")
-        let select = document.querySelector("#vernis")
-        let achat = document.querySelector("#achat")
-        let prix = document.querySelector("#prix")
-        let btnAjout = document.querySelector("#btn-panier-ajout")
-        let btnSupp = document.querySelector("#btn-panier-supp")
-        let nbrArticles = document.querySelector("#nbr-articles")
-        let totalArticles = 0
-        let btnValid = document.querySelector("#btn-panier-valid")
-        //text pour commande quantité nul
-        let spanError = document.createElement("span")
-        spanError.innerHTML = " "
-        document.querySelector("#modif-achat").appendChild(spanError)
+//selection des elements 
+let pageMeuble = document.querySelector("#page-meuble")       
+let h1Meuble = document.querySelector("#id-titre");
+let imageMeuble = document.querySelector("#img")
+let description = document.querySelector("#description")
+let select = document.querySelector("#vernis")
+let achat = document.querySelector("#achat")
+let prix = document.querySelector("#prix")
+let btnAjout = document.querySelector("#btn-panier-ajout")
+let btnSupp = document.querySelector("#btn-panier-supp")
+btnSupp.setAttribute("disabled", false)
+let nbrArticles = document.querySelector("#nbr-articles")
+let totalArticles = 0
+let btnValid = document.querySelector("#btn-panier-valid")
+let titlePage = document.querySelector("title")
 
-    
-//creation de la liste  des differents vernis
-        for (let eltVernis of meuble.varnish){
+//text pour commande quantité nul
+let spanError = document.createElement("span")
+spanError.innerHTML = " "
+document.querySelector("#modif-achat").appendChild(spanError)
 
-            let option = document.createElement("option");
-            option.setAttribute("value", eltVernis);
-            option.innerHTML = eltVernis
-            select.appendChild(option)
+
+
+const getFurniture = async function(){
+    try{
+        let response = await fetch( "http://localhost:3000/api/furniture/" + id)//renvoi une promesse
+        if(response.ok){
+            let meuble = await response.json()//renvoi une promesse
+            console.log(meuble)
+            titlePage.innerHTML = meuble.name
+
+
+    //creation de la liste  des differents vernis
+            for (let eltVernis of meuble.varnish){
+
+                let option = document.createElement("option");
+                option.setAttribute("value", eltVernis);
+                option.innerHTML = eltVernis
+                select.appendChild(option)
+            }
+
+            h1Meuble.textContent = meuble.name
+            
+            imageMeuble.setAttribute("src", meuble.imageUrl)
+            imageMeuble.setAttribute("alt", meuble.description)
+            
+            description.textContent = meuble.description
+
+            prix.textContent = meuble.price + " €"
+            nbrArticles.innerHTML = 0   
+
+            
+    //fonction sur le bouton pour ajouter au panier
+            btnAjout.addEventListener("click", function(){
+                
+    //verifie si l'article n'a pas deja été ajouté           
+                if(["ligne-panier_"+meuble._id+"-"+select.value] in localStorage){
+                    btnAjout.setAttribute("disabled", false)
+                    btnValid.setAttribute("disabled", false)
+                    
+                    spanError.innerHTML = "article déja ajouté au panier"
+                }
+                else{
+
+                    totalArticles = 1; //car pour le MVP pas plus d'une ref pour l'ajout au panier
+                    nbrArticles.innerHTML = totalArticles 
+                    btnSupp.setAttribute("disabled", false)
+                    spanError.innerHTML = " "
+                }
+            })
+    //fonction sur le bouton pour supp du panier avec disabled pour chiffre < 0
+            btnSupp.addEventListener("click", function(){
+                
+                if (totalArticles > 0) {
+                    totalArticles--;
+                    nbrArticles.innerHTML = totalArticles
+
+                    
+                }else {
+                    btnSupp.setAttribute("disabled", true)
+                }
+            })  
+            
+            btnValid.addEventListener("click", function(e){
+                if (totalArticles>=1){
+                arrayMeuble.push(meuble.name, select.value, totalArticles, meuble.price*parseInt(totalArticles) +" €", meuble._id)
+                console.log(arrayMeuble)  
+
+                    if(["ligne-panier_"+meuble._id+"-"+select.value] in localStorage){
+                        spanError.innerHTML = "article déja ajouté au panier"
+                    }else{
+                        localStorage.setItem("ligne-panier_"+meuble._id+"-"+select.value, arrayMeuble)
+                        spanError.innerHTML = "Article ajouté au panier "
+                        btnValid.setAttribute("disabled", true)
+                    }
+                }    
+                else {
+                    //e.preventDefault()
+                    spanError.innerHTML = "Aucune quantité commandé"
+                    console.log("aucune quantité commandé")
+                }   
+            })
+        }else{
+            console.error("Probleme acces serveur : ", response.status)
         }
 
-        h1Meuble.textContent = meuble.name
-        
-        imageMeuble.setAttribute("src", meuble.imageUrl)
-        imageMeuble.setAttribute("alt", meuble.description)
-        
-        description.textContent = meuble.description
-
-        prix.textContent = meuble.price + " €"
-        nbrArticles.innerHTML = 0   
-        
-        
-
-         
-//fonction sur le bouton pour ajouter au panier
-        btnAjout.addEventListener("click", function(){
-
-//verifie si             
-            if(["ligne-panier_"+meuble._id+"-"+select.value] in localStorage){
-                btnValid.setAttribute("disabled", true)
-                spanError.innerHTML = "article déja ajouté au panier"
-            }
-            else{
-
-                totalArticles = 1; //car pour le MVP pas plus d'une ref pour l'ajout au panier
-                nbrArticles.innerHTML = totalArticles 
-                btnSupp.disabled = false
-                spanError.innerHTML = " "
-            }
-        })
-//fonction sur le bouton pour supp du panier avec disabled pour chiffre < 0
-        btnSupp.addEventListener("click", function(){
-            
-            if (totalArticles > 0) {
-                totalArticles--;
-                nbrArticles.innerHTML = totalArticles 
-                
-            }else {
-                btnSupp.disabled = true
-            }
-        })  
-        
-        btnValid.addEventListener("click", function(e){
-            if (totalArticles>=1){
-            arrayMeuble.push(meuble.name, select.value, totalArticles, meuble.price*parseInt(totalArticles) +" €", meuble._id)
-            console.log(arrayMeuble)  
-
-                if(["ligne-panier_"+meuble._id+"-"+select.value] in localStorage){
-                    //e.preventDefault()
-                    //alert("coucou")
-                    spanError.innerHTML = "article déja ajouté au panier"
-                }else{
-                    localStorage.setItem("ligne-panier_"+meuble._id+"-"+select.value, arrayMeuble)
-                    spanError.innerHTML = "Article ajouté au panier "
-                    btnValid.setAttribute("disabled", true)
-                }
-            }    
-            else {
-                //e.preventDefault()
-                spanError.innerHTML = "Aucune quantité commandé"
-                console.log("aucune quantité commandé")
-            }   
-        })
-    }
-           
+    }catch(e){
+        console.error ("probleme avec le fetch "+e)
+    }   
 }
-request.open ("GET", "http://localhost:3000/api/furniture/" + id)
-request.send()
-
-
-// btnValid.addEventListener("click", function(e){
-//     if (totalArticles>=1){
-//     arrayMeuble.push(meuble.name, select.value, totalArticles, meuble.price*parseInt(totalArticles) +" €", meuble._id)
-//     console.log(arrayMeuble)  
-//     localStorage.setItem("ligne-panier_"+meuble._id+"-"+select.value, arrayMeuble)
-//     spanError.innerHTML = "Article ajouté au panier "
-//     btnValid.setAttribute("disabled", true)
-
-//     }else if(["ligne-panier_"+meuble._id+"-"+select.value] in localStorage){
-//         //e.preventDefault()
-//         alert("coucou")
-//         spanError.innerHTML = "article déja ajouté au panier"
-//     }else {
-//         //e.preventDefault()
-//         spanError.innerHTML = "Aucune quantité commandé"
-//         console.log("aucune quantité commandé")
-//     }   
-// })
+getFurniture()
